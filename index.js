d5e390dff86a3ad327e2a7e816d9d2ac9d360698
Samuel Ebenezer <samuelebenezer1998@gmail.com>
2024-10-19 01:15:34 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const bibleRoutes = require('./routes/bibleRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// API Routes
+app.use('/api', bibleRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
