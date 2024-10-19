bd4544911837713ac237cd911e2d0426e85d7480
Samuel Ebenezer <samuelebenezer1998@gmail.com>
2024-10-19 20:54:28 +0530

    netlify

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const bibleRoutes = require('./routes/bibleRoutes');
+const bibleRoutes = require('./bibleRoutes');
 const serverless = require('serverless-http');
 
 
